68bf6a9941f50645e488de60cea940aa2e331322
Gregory Oppong <100242161+kofirockerfeller@users.noreply.github.com>
2025-06-19 17:15:56 +0000

    e tracker app summer, first

---
+++ expense-tracker-app/App.js
@@ -0,0 +1,15 @@
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+import Home from './components/Home';
+
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+    <Home/>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
